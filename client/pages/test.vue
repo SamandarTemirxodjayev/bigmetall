9c472abdf6522ef5a7180122edd38cf6529a9001
Samandar <iwtuz166@gmail.com>
2024-02-05 17:55:02 +0500

    for test

--- client/pages/test.vue
+++ client/pages/test.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <VueNumber
-      v-model="price"
-      v-bind="number"
-      class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-    />
-  </div>
+  <div>Test is work</div>
 </template>
-
-<script setup>
-import { component as VueNumber } from "@coders-tm/vue-number-format";
-
-let number = ref({
-  decimal: ".",
-  separator: " ",
-  suffix: " so'm",
-  precision: 2,
-  masked: false,
-});
-let price = ref();
-</script>
