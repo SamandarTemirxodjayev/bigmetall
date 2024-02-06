1bc8b6f5b1ee8bc8599734bcea5cf461b95e20ce
Samandar <iwtuz166@gmail.com>
2024-02-06 22:01:30 +0500

    changeIP

--- client/composables/axios.js
+++ client/composables/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/api/";
+const API_URL = "http://188.92.28.212:3001/api/";
 let accessToken = null;
 let headers = {
   "Content-Type": "application/json",
